cfa3028d3d66a9666bd9c88c5940cf97fecfd233
Emerson Pedroso <emerson@bitcoinnano.org>
2024-02-10 14:09:50 -0300

    fix s3 host

--- amazon_s3_connector/models/amazon_dashboard.py
+++ amazon_s3_connector/models/amazon_dashboard.py
@@ -1,24 +1,3 @@
-# -*- coding: utf-8 -*-
-###############################################################################
-#
-#   Cybrosys Technologies Pvt. Ltd.
-#
-#   Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
-#   Author: Aslam A K( odoo@cybrosys.com )
-#
-#   You can modify it under the terms of the GNU AFFERO
-#   GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
-#
-#   You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
-#   (AGPL v3) along with this program.
-#   If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
 import boto3
 import os
 from odoo import models
@@ -28,49 +7,66 @@ class AmazonDashboard(models.Model):
     """
     Amazon dashboard Model to connect with amazon S3
     """
-    _name = 'amazon.dashboard'
+
+    _name = "amazon.dashboard"
     _description = "Amazon Dashboard"
 
     def amazon_view_files(self):
         """
         Fetch all files from s3 and returns it.
         """
-        access_key = self.env['ir.config_parameter'].get_param(
-            'amazon_s3_connector.amazon_access_key')
-        access_secret = self.env['ir.config_parameter'].get_param(
-            'amazon_s3_connector.amazon_secret_key')
-        bucket_name = self.env['ir.config_parameter'].get_param(
-            'amazon_s3_connector.amazon_bucket_name')
+        access_key = self.env["ir.config_parameter"].get_param(
+            "amazon_s3_connector.amazon_access_key"
+        )
+        access_secret = self.env["ir.config_parameter"].get_param(
+            "amazon_s3_connector.amazon_secret_key"
+        )
+        bucket_name = self.env["ir.config_parameter"].get_param(
+            "amazon_s3_connector.amazon_bucket_name"
+        )
+        custom_host = self.env["ir.config_parameter"].get_param(
+            "amazon_s3_connector.custom_host"
+        )  # Adicione esta linha
+
         if not access_key or not access_secret or not bucket_name:
             return False
         try:
-            client = boto3.client('s3', aws_access_key_id=access_key,
-                                  aws_secret_access_key=access_secret)
-            region = client.get_bucket_location(Bucket=bucket_name)
             client = boto3.client(
-                's3', region_name=region['LocationConstraint'],
+                "s3",
                 aws_access_key_id=access_key,
-                aws_secret_access_key=access_secret
-            )
+                aws_secret_access_key=access_secret,
+                endpoint_url=custom_host,
+            )  # Modifique esta linha
+            region = client.get_bucket_location(Bucket=bucket_name)[
+                "LocationConstraint"
+            ]
+            client = boto3.client(
+                "s3",
+                region_name=(
+                    region if region else "us-east-1"
+                ),  # Adicione condicional para 'us-east-1' como fallback
+                aws_access_key_id=access_key,
+                aws_secret_access_key=access_secret,
+                endpoint_url=custom_host,
+            )  # Modifique esta linha novamente
             response = client.list_objects(Bucket=bucket_name)
             file = []
-            for data in response['Contents']:
+            for data in response.get("Contents", []):
                 url = client.generate_presigned_url(
-                    ClientMethod='get_object',
-                    Params={'Bucket': bucket_name, 'Key': data['Key']})
-                if data['Size'] == 0:
+                    ClientMethod="get_object",
+                    Params={"Bucket": bucket_name, "Key": data["Key"]},
+                )
+                if data["Size"] == 0:
                     continue
-                size_bytes = data['Size'] / 1024
+                size_bytes = data["Size"] / 1024
                 if size_bytes > 1024:
-                    size = str(
-                        round(data['Size'] / (1024 * 1024), 1)) + ' MB'
+                    size = str(round(data["Size"] / (1024 * 1024), 1)) + " MB"
                 else:
-                    size = str(round(data['Size'] / 1024, 1)) + ' KB'
-                file_type = str.upper(
-                    os.path.splitext(data['Key'])[1].replace('.', ''))
+                    size = str(round(data["Size"] / 1024, 1)) + " KB"
+                file_type = str.upper(os.path.splitext(data["Key"])[1].replace(".", ""))
                 file.append(
-                    [data['Key'], url, file_type,
-                     str(data['LastModified']), size])
+                    [data["Key"], url, file_type, str(data["LastModified"]), size]
+                )
             return file
         except Exception as e:
-            return ['e', e]
+            return ["e", e]
--- amazon_s3_connector/models/res_config_settings.py
+++ amazon_s3_connector/models/res_config_settings.py
@@ -1,42 +1,41 @@
-# -*- coding: utf-8 -*-
-###############################################################################
-#
-#   Cybrosys Technologies Pvt. Ltd.
-#
-#   Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
-#   Author: Aslam A K( odoo@cybrosys.com )
-#
-#   You can modify it under the terms of the GNU AFFERO
-#   GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
-#
-#   You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
-#   (AGPL v3) along with this program.
-#   If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
 from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
     """
-    Configure the access credentials
+    Configure the access credentials and endpoint URL for Amazon S3 or compatible services
     """
-    _inherit = 'res.config.settings'
 
-    amazon_access_key = fields.Char(string='Amazon S3 Access Key', copy=False,
-                                    config_parameter='amazon_s3_connector.amazon_access_key',
-                                    help='Enter your Amazon S3 Access Key here.')
-    amazon_secret_key = fields.Char(string='Amazon S3 Secret key',
-                                    config_parameter='amazon_s3_connector.amazon_secret_key',
-                                    help='Enter your Amazon S3 Secret Key here.')
-    amazon_bucket_name = fields.Char(string='Folder ID',
-                                     config_parameter='amazon_s3_connector.amazon_bucket_name',
-                                     help='Enter the name of your Amazon S3 Bucket here.')
+    _inherit = "res.config.settings"
+
+    amazon_access_key = fields.Char(
+        string="Amazon S3 Access Key",
+        copy=False,
+        config_parameter="amazon_s3_connector.amazon_access_key",
+        help="Enter your Amazon S3 Access Key here.",
+    )
+
+    amazon_secret_key = fields.Char(
+        string="Amazon S3 Secret key",
+        config_parameter="amazon_s3_connector.amazon_secret_key",
+        help="Enter your Amazon S3 Secret Key here.",
+    )
+
+    amazon_bucket_name = fields.Char(
+        string="Folder ID",
+        config_parameter="amazon_s3_connector.amazon_bucket_name",
+        help="Enter the name of your Amazon S3 Bucket here.",
+    )
+
     is_amazon_connector = fields.Boolean(
-        config_parameter='amazon_s3_connector.amazon_connector', default=False,
-        help='Enable or disable the Amazon S3 connector.')
+        config_parameter="amazon_s3_connector.amazon_connector",
+        default=False,
+        help="Enable or disable the Amazon S3 connector.",
+    )
+
+    # Adiciona uma nova configuração para o host customizado
+    amazon_custom_host = fields.Char(
+        string="Custom S3 Host",
+        config_parameter="amazon_s3_connector.custom_host",
+        help="Enter your custom S3 Host URL here if you are not using Amazon S3.",
+    )
